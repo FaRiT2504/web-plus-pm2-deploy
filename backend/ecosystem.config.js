b4916c30fe8a4c28734004d10ad92d9830abbecc
Farit <Faritplus@yandex.ru>
2024-09-11 22:20:03 +0500

    исправил ecosystem.config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_PATH, DEPLOY_REPOSITORY
+} = process.env;
+
 module.exports = {
-  apps : [{
-    name   : "app1",
-    script : "./app.js"
-  }]
-}
+  apps: [
+    {
+      name: 'mesto',
+      script: 'dist/app.js',
+    },
+  ],
+
+  deploy: {
+    production: {
+      NODE_ENV: 'production',
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy':
+        'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
